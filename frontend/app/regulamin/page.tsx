297fd5fab1420e9c485943423d029acfffa6e9f7
Francky <contact@francky.pl>
2024-02-21 14:05:36 +0100

    fix text color on light mode

--- frontend/app/regulamin/page.tsx
+++ frontend/app/regulamin/page.tsx
@@ -2,7 +2,7 @@ const heading = "Regulamin";
 
 function Regulamin() {
   return (
-    <section className="mx-8 lg:mx-24 2xl:mx-80 lg:px-24 pb-24 text-neutral-400">
+    <section className="mx-8 lg:mx-24 2xl:mx-80 lg:px-24 pb-24 dark:text-neutral-400">
       <h2
         id="szkolenie"
         className="p-8 pl-8 lg:my-16 lg:w-1/3 text-3xl lg:text-5xl mr-auto bg-gradient-to-r from-neutral-50 dark:from-black to-orange-400 dark:to-orange-400 text-black dark:text-white text-center"
