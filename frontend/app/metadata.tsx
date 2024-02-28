c75bd55e08d158da07dd73b0b2646f9aa5b3c1d8
Francky <contact@francky.pl>
2024-02-28 11:39:21 +0100

    remove unused file

--- frontend/app/metadata.tsx
+++
@@ -1,7 +0,0 @@
-import { Metadata } from "next/types";
-
-export const metadata: Metadata = {
-  title: "Primo MOS Barber Shop - Gdańsk",
-  description:
-    "Gdzie pasja i profesjonalizm spotykają się, by zapewnić Ci niezapomniane doświadczenie fryzjerskie. Nasza misja to nie tylko strzyżenie, ale tworzenie stylu, który podkreśli Twój indywidualizm i charakter",
-};
