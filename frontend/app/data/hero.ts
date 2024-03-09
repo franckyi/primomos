8081d37a0331c84681043a93e0e58df760e9ae1f
Francky <contact@francky.pl>
2024-03-09 16:23:34 +0100

    fix text colors

--- frontend/app/data/hero.ts
+++ frontend/app/data/hero.ts
@@ -2,13 +2,13 @@ const hero = {
   headingClasses:
     "max-md:mx-auto max-md:text-center lg:ml-80 text-3xl lg:text-7xl font-bold uppercase",
   subheadingClasses:
-    "text-neutral-500 dark:text-neutral-700 lg:hover:translate-x-4 transition duration-300",
+    "text-neutral-700 lg:hover:translate-x-4 transition duration-300",
   line1: {
     text: "Witaj w",
   },
   line2: {
     classes:
-      "max-md:my-4 text-5xl lg:text-9xl text-neutral-800 dark:text-orange-400 lg:hover:translate-x-4 transition duration-300",
+      "max-md:my-4 text-5xl lg:text-9xl text-orange-400 lg:hover:translate-x-4 transition duration-300",
   },
   line3: {
     text: "Barber Shop",
