913492ef83a37cea320ebfcfe8d6f63450d91c3d
Francky <contact@francky.pl>
2024-04-05 20:16:36 +0200

    prevent elements overlapping

--- frontend/app/kontakt/page.tsx
+++ frontend/app/kontakt/page.tsx
@@ -2,7 +2,7 @@ import ContactForm from "../ui/contact/contact-form";
 
 export default function Contact() {
   return (
-    <div className="min-h-screen p-8 lg:px-24 lg:py-16 text-center">
+    <div className="min-h-screen p-8 max-md:mt-[200px] lg:px-24 lg:py-16 text-center">
       <ContactForm />
     </div>
   );
